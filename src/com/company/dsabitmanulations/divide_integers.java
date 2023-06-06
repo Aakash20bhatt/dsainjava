f1ffcd3eb1d133b413b20565bf9c588dc41073de
aakashbhatt <aakashbhatt.19@gmail.com>
2023-06-07 00:46:53 +0530

    07June

--- src/com/company/dsabitmanulations/divide_integers.java
+++ src/com/company/dsabitmanulations/divide_integers.java
@@ -12,6 +12,33 @@ package com.company.dsabitmanulations;
 public class divide_integers {
 
     public int solve(int A, int B){
+        long n = A, m = B;
+        int sign = 1;
 
+        if((n<0 && m>=0)||(m<0 && n>=0)){
+            sign=-1;
+        }
+
+        n = Math.abs(n);
+        m = Math.abs(m);
+
+        long q  = 0, t = 0;
+
+        for(int i=31; i>=0; i--){
+            if(t+(m<<i)<=n){
+                t+=m<<i;
+                q |= (long)1<<i;
+            }
+        }
+
+        if(sign<0){
+            q=-q;
+        }
+
+        if(q>Integer.MAX_VALUE){
+            return Integer.MAX_VALUE;
+        }else{
+            return (int)q;
+        }
     }
 }

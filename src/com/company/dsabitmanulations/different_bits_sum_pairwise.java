f98d04b036d99d965d4bb194bb4cc4c82479516f
aakashbhatt <aakashbhatt.19@gmail.com>
2023-06-06 23:54:41 +0530

    questionadded

--- src/com/company/dsabitmanulations/different_bits_sum_pairwise.java
+++ src/com/company/dsabitmanulations/different_bits_sum_pairwise.java
@@ -1,5 +1,13 @@
 package com.company.dsabitmanulations;
 
+// Problem Description
+// We define f(X, Y) as the number of different corresponding bits in the binary representation of X and Y.
+// For example, f(2, 7) = 2, since the binary representation of 2 and 7 are 010 and 111, respectively. The first and the
+// third bit differ, so f(2, 7) = 2.
+
+// You are given an array of N positive integers, A1, A2,..., AN. Find sum of f(Ai, Aj) for all pairs (i, j) such that
+// 1 ≤ i, j ≤ N. Return the answer modulo 10^9+7.
+
 public class different_bits_sum_pairwise {
 
     public int solve(int[] A){
---
+++ src/com/company/dsabitmanulations/divide_integers.java
@@ -0,0 +1,17 @@
+package com.company.dsabitmanulations;
+
+// Problem Description
+// Divide two integers without using multiplication, division and mod operator.
+
+// Return the floor of the result of the division.
+
+// Also, consider if there can be overflow cases i.e output is greater than INT_MAX, return INT_MAX.
+
+// NOTE: INT_MAX = 2^31 - 1
+
+public class divide_integers {
+
+    public int solve(int A, int B){
+
+    }
+}
